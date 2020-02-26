d2d32faa9f944cf40ee62e9aab7c4c0ee695530b
Liby Lee <liby99@icloud.com>
2020-02-26 04:23:16 -0500

    Add more tests

---
+++ tests/fields_tests/fields_in_functor.cpp
@@ -0,0 +1,36 @@
+#include "math/prelude.hpp"
+#include "storage/prelude.hpp"
+
+using namespace storage;
+
+using namespace math;
+
+template <typename... Types>
+using BaseStorage = Storage<Kokkos::Serial, Kokkos::HostSpace, 4, Types...>;
+
+template <typename T, int dim>
+using MyStorage = BaseStorage<T, Vector<T, dim>, Vector<T, dim>>;
+
+template <std::size_t Offset, typename T, int dim>
+struct storage::FieldsWithOffset<Offset, MyStorage<T, dim>> {
+  enum Indices { M, V, X };
+};
+
+template <typename T, int dim>
+struct Functor {
+  using Storage = MyStorage<T, dim>;
+
+  static std::unique_ptr<Storage> create() {
+    return std::make_unique<Storage>();
+  }
+};
+
+void test() {
+  auto store = Functor<float, 3>::create();
+}
+
+int main() {
+  Kokkos::initialize();
+  test();
+  Kokkos::finalize();
+}
