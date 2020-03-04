be0d1d4661d3f53f1d0094c5bf6857d6ac9617c2
Ziyang Li <liby99@seas.upenn.edu>
2020-03-04 18:48:17 -0500

    Can run pure cuda code

---
+++ bench/config.hpp
@@ -0,0 +1,24 @@
+#include <Cabana_Core.hpp>
+
+// Execution space
+#if defined STORAGE_ENABLED_CUDA
+  using ExecutionSpace = Kokkos::Cuda;
+#elif defined STORAGE_ENABLED_OPENMP
+  using ExecutionSpace = Kokkos::OpenMP;
+#else
+  using ExecutionSpace = Kokkos::Serial;
+#endif
+
+// Memory space & Bin Size
+#if defined STORAGE_ENABLE_CUDA
+  using MemorySpace = Kokkos::CudaSpace;
+
+  static const std::size_t BIN_SIZE = 32;
+#else
+  using MemorySpace = Kokkos::HostSpace;
+
+  static const std::size_t BIN_SIZE = 4;
+#endif
+
+// Device type
+using KokkosDevice = Kokkos::Device<ExecutionSpace, MemorySpace>;
