7750313254c8dd45c7e413dbb1b247c780a8101e
Ziyang Li <liby99@seas.upenn.edu>
2020-03-05 12:55:22 -0500

    Add dummy grid

--- bench/cabana_mpm/cabana_mpm.cpp
+++ bench/cabana_mpm/cabana_mpm.cpp
@@ -1,6 +1,7 @@
-#include "./kernels/initialize_particles.hpp"
-#include "./storages.hpp"
 #include <random>
+
+#include <./grid.hpp>
+#include <./storages.hpp>
 
 template <class T>
 T random() {
@@ -49,13 +50,47 @@ void initialize_particles(Particles<T, D> &particles, int start, int amount) {
 }
 
 template <class T, int D>
+void initialize_stresses(Stresses<T, D> &stresses, int start, int amount) {
+  auto slice_ks = Cabana::slice<0>(stresses);
+  auto slice_ls = Cabana::slice<1>(stresses);
+
+  auto kernel = KOKKOS_LAMBDA(const int s, const int a) {
+    for (int j = 0; j < D; j++) {
+      for (int k = 0; k < D; k++) {
+        slice_ks.access(s, a, j, k) = 0.0;
+      }
+    }
+
+    for (int j = 0; j < D; j++) {
+      slice_ls.access(s, a, j) = 0.0;
+    }
+  };
+
+  Cabana::SimdPolicy<BIN_SIZE, ExecutionSpace> simd_policy(start, amount);
+  Cabana::simd_parallel_for(simd_policy, kernel, "initialize_stresses");
+}
+
+template <class T, int D>
+void p2g(Particles<T, D> &particles,
+         Stresses<T, D> &stresses,
+         Grid<T, D> &grid,
+         T[D] gravity,
+         T dx,
+         T dt,
+         int start,
+         int amount) {}
+
+template <class T, int D>
 void run() {
   const int particle_count = 100000000;
+
+  // Initialize storages
   Particles<T, D> particles("particles", particle_count);
   Stresses<T, D> stresses("stresses", particle_count);
 
   // Initialize particles
   initialize_particles<T, D>(particles, 0, particle_count);
+  initialize_stresses<T, D>(stresses, 0, particle_count);
 }
 
 int main() {
---
+++ bench/cabana_mpm/grid.hpp
@@ -0,0 +1,10 @@
+#include <../config.hpp>
+
+template <class T, int D>
+struct Grid {
+  Kokkos::View<T * [D + 1][64], KokkosDevice> data;
+  Kokkos::View<uint64_t *, KokkosDevice> offsets;
+  Kokkos::UnorderedMap<uint64_t, size_t, KokkosDevice> mapping;
+
+  Grid() : data("grid_data", 100), offsets("grid_offsets", 100), mapping() {}
+};
